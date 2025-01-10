2971e9b4df54b390bd690bde3f6d4676bc398b1a
anant2704 <142739416+anant2704@users.noreply.github.com>
2025-01-10 22:08:29 +0530

    Delete config.php

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "donation_management";
-
-// Create connection
-$conn = mysqli_connect($servername, $username, $password, $dbname);
-?>
